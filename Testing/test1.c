6eb3c803225863a682026ecb1e90c95c3dbc6b72
lpinilla <lauti_nipi123@hotmail.com>
2019-03-22 16:05:31 -0300

    finalizada version 1 del framework, falta ver que pasa si se cuelgan los tests y agregar mas tests y checkear con valgrind todo

--- Testing/test1.c
+++ Testing/test1.c
@@ -1,6 +1,89 @@
 #include "testing_suite.h"
 
+void test1(void);
+void test2(void);
+void test3(void);
+void test4(void);
+void test5(void);
+void test6(void);
+void test7(void);
+void test8(void);
+void test9(void);
+void test10(void);
+//faltan tests
 
 int main(void){
-   printf("it works");
+   //creando un test_suite
+    test_suite my_suite = create_suite(10, "Testing the Framework");
+
+    add_test(my_suite, test1);
+    add_test(my_suite, test2);
+    add_test(my_suite, test3);
+    add_test(my_suite, test4);
+    add_test(my_suite, test5);
+    add_test(my_suite, test6);
+    add_test(my_suite, test7);
+    add_test(my_suite, test8);
+    add_test(my_suite, test9);
+    add_test(my_suite, test10);
+
+    run_suite(my_suite);
+    
+    free(my_suite->fun_ptrs);
+    free(my_suite);
 }
+
+void test1(){
+    printf("hello \n");
+    return;
+}
+
+void test2(){
+    printf("world! \n");
+    return;
+}
+
+//todos buenos
+
+void test3(){
+    assert_true(1 == 1);
+}
+
+void test4(){
+    assert_false(1 == 2);
+}
+
+void test5(){
+    int a = 1;
+    int b = 1;
+    assert_equals(&a, &b, sizeof(int));
+}
+
+void test6(){
+    int a = 1;
+    int b = 2;
+    assert_not_equals(&a,&b, sizeof(int));
+}
+
+//ahora los malos
+
+void test7(){
+    assert_true(1 == 0);
+}
+
+void test8(){
+    assert_false(1 == 1);
+}
+
+void test9(){
+    int a = 1;
+    int b = 2;
+    assert_equals(&a, &b, sizeof(int));
+}
+void test10(){
+    int a = 1;
+    int b = 1;
+    assert_not_equals(&a,&b, sizeof(int));
+}
+
+//FALTA TESTEAR
--- Testing/testing_suite.c
+++ Testing/testing_suite.c
@@ -1,44 +1,5 @@
 #include "testing_suite.h"
 
-//testing suite to test the framework
-
-
-void test1(void);
-void test2(void);
-void test3(void);
-void test4(void);
-void test5(void);
-void test6(void);
-void test7(void);
-void test8(void);
-void test9(void);
-void test10(void);
-//faltan tests
-
-
-
-int main(void){
-    //creando un test_suite
-    test_suite my_suite = create_suite(10, "Testing the Framework");
-
-    add_test(my_suite, test1);
-    add_test(my_suite, test2);
-    add_test(my_suite, test3);
-    add_test(my_suite, test4);
-    add_test(my_suite, test5);
-    add_test(my_suite, test6);
-    add_test(my_suite, test7);
-    add_test(my_suite, test8);
-    add_test(my_suite, test9);
-    add_test(my_suite, test10);
-
-    run_suite(my_suite);
-    
-    free(my_suite->fun_ptrs);
-    free(my_suite);
-}
-
-
 test_suite create_suite(int n_of_tests, char * suite_name){
     test_suite my_suite = (t_test_suite *) malloc(sizeof(t_test_suite));
     my_suite->n_of_tests = n_of_tests;
@@ -103,61 +64,6 @@ void run_suite(test_suite suite){
     printf("----------------------------------------------\n");
 }
 
-
-void test1(){
-    printf("hello \n");
-    return;
-}
-
-void test2(){
-    printf("world! \n");
-    return;
-}
-
-//todos buenos
-
-void test3(){
-    assert_true(1 == 1);
-}
-
-void test4(){
-    assert_false(1 == 2);
-}
-
-void test5(){
-    int a = 1;
-    int b = 1;
-    assert_equals(&a, &b, sizeof(int));
-}
-
-void test6(){
-    int a = 1;
-    int b = 2;
-    assert_not_equals(&a,&b, sizeof(int));
-}
-
-//ahora los malos
-
-void test7(){
-    assert_true(1 == 0);
-}
-
-void test8(){
-    assert_false(1 == 1);
-}
-
-void test9(){
-    int a = 1;
-    int b = 2;
-    assert_equals(&a, &b, sizeof(int));
-}
-void test10(){
-    int a = 1;
-    int b = 1;
-    assert_not_equals(&a,&b, sizeof(int));
-}
-
-//FALTA TESTEAR
 
 void assert_equals(const void * a, const void * b, size_t size_of_data){
     if((a == NULL) && (b == NULL)) {
