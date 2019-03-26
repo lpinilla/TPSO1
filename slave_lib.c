ea850e97d30ceb7bc7c36daf377cac89779596eb
lpinilla <lauti_nipi123@hotmail.com>
2019-03-26 17:19:23 -0300

    hecho caso de si malloc falla en process_hash

--- slave_lib.c
+++ slave_lib.c
@@ -14,11 +14,20 @@ void read_file_names(Queue q){
 char * process_hash(char * file_name){
 	char * command_name = "md5sum ";
 	//combinar para que sea "md5sum file_name"
-	char * full_command = (char *)malloc((strlen(command_name) + strlen(file_name) + 2) * sizeof(char));  //malloc puede fallar
+	char * full_command = (char *) malloc((strlen(command_name) + strlen(file_name) + 1) * sizeof(char));  //malloc puede fallar
+	if(full_command == NULL){
+		printf("Malloc failed, not enough memory");
+		return NULL;
+	}
 	full_command = strcat(full_command, command_name);
 	full_command = strcat(full_command, file_name);
     //hay que liberarlo
 	char * command_output = (char *) malloc(MD5LENGTH * sizeof(char)); 
+	if(command_output == NULL){
+		printf("Malloc failed, not enough memory");
+		free(full_command);
+		return NULL;
+	}
 	call_command(full_command, command_output);
 	free(full_command);
 	return command_output;
