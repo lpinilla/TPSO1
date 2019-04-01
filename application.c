c1013331e097a127882bf64780798208bce05600
JoaquinBattilana <battilanajn@gmail.com>
2019-04-01 15:59:30 -0300

    Changed args reading from main to a function

--- application.c
+++ application.c
@@ -6,17 +6,59 @@
 
 #define NUMBER_OF_SLAVES 5
 
+void enqueue_args(Queue *, int, char **);
 
 int main(int argc, char ** argv){
     // si no tenemos argumentos no hay nada que hacer
-    if(argc==1){
+    if(argc==0 || argc == 1){
         return 1;
     }
-    // creamos la estructura stat
-    struct stat path_stat;
-    // creamos la cola que va a contener a los files
+
     Queue * files = newQueue();
     queueInit(files, sizeof(char*));
+
+    enqueue_args(files, argc, argv);
+
+    //imprimir el pid para vision
+    printf("%d", getpid());
+    int n_of_files = 1;
+    void ** shm_ptr = create_shared_memory(n_of_files * sizeof(void *));
+    shm_info mem_info = initialize_shared_memory(shm_ptr, n_of_files);
+    
+    printf("%p \n", mem_info); //para que me deje compilar
+
+    // creamos los fd para los pipes de ida
+    int pipes[NUMBER_OF_SLAVES][2];
+    // abrimos todos los pipes de ida
+    for(int i=0; i<NUMBER_OF_SLAVES; i++){
+        pipe(pipes[i]);
+    }
+    // el padre de por si cierra su stdout
+    for(int i=0; i<NUMBER_OF_SLAVES; i++){
+        if(fork()==0){
+            // el hijo cierra su stdin
+            close(0);
+            // lo redireccionamos al pipe correspondiente
+            dup(pipes[i][0]);
+            // para testear usamos esto desp vemos el nombre 
+            execv("./slave.out","");
+        }
+    }
+    // el padre les escribe los archivos
+    for(int i=0; i<NUMBER_OF_SLAVES; i++){
+        write(pipes[i][1], "slave.c\n",8);
+        write(pipes[i][1], "slave.h\n",8);
+    }
+
+    //desvincularse a la memoria y liberarla
+    clear_shared_memory(shm_ptr, n_of_files, mem_info);
+
+}
+
+void enqueue_args(Queue * files, int argc, char ** argv){
+        // creamos la estructura stat
+    struct stat path_stat;
+    // creamos la cola que va a contener a los files
     for(int i=1; i<argc; i++){
         char * filename = malloc(sizeof(char)*strlen(argv[i]));
         strcpy(filename,argv[i]);
@@ -57,43 +99,4 @@ int main(int argc, char ** argv){
         dequeue(files, &aux);
         printf("%s\n", aux);
     }
-
-
-    /*
-
-    //imprimir el pid para vision
-    printf("%d", getpid());
-    int n_of_files = 1;
-    void ** shm_ptr = create_shared_memory(n_of_files * sizeof(void *));
-    shm_info mem_info = initialize_shared_memory(shm_ptr, n_of_files);
-    
-    printf("%p \n", mem_info); //para que me deje compilar
-
-    // creamos los fd para los pipes de ida
-    int pipes[NUMBER_OF_SLAVES][2];
-    // abrimos todos los pipes de ida
-    for(int i=0; i<NUMBER_OF_SLAVES; i++){
-        pipe(pipes[i]);
-    }
-    // el padre de por si cierra su stdout
-    for(int i=0; i<NUMBER_OF_SLAVES; i++){
-        if(fork()==0){
-            // el hijo cierra su stdin
-            close(0);
-            // lo redireccionamos al pipe correspondiente
-            dup(pipes[i][0]);
-            // para testear usamos esto desp vemos el nombre 
-            execv("./slave.out", NULL);
-        }
-    }
-    // el padre les escribe los archivos
-    for(int i=0; i<NUMBER_OF_SLAVES; i++){
-        write(pipes[i][1], "slave.c\n",8);
-        write(pipes[i][1], "slave.h\n",8);
-    }
-
-    //desvincularse a la memoria y liberarla
-    clear_shared_memory(shm_ptr, n_of_files, mem_info);
-
-    */
 }
