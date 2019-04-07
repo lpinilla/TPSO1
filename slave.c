7cdaf993f8d53a576d17f785de207bfd507ed23b
JoaquinBattilana <battilanajn@gmail.com>
2019-04-07 13:14:26 -0300

    changed slave logic to slave.c and created standars fd array

--- slave.c
+++ slave.c
@@ -1,5 +1,19 @@
 #include "slave.h"
 
 int main(void){
-    
+    int standars[2];
+    standars[0] = stdin;
+    standars[1] = stdout;
+    //El hijo entra en ciclo hasta que el padre le indique que cierre
+    while(1){
+        char * msg = read_pipe(standars);
+        if(msg != NULL){
+            if(*msg == 0){
+                exit(0);
+            }else{
+                load_file(msg, standars);
+            }
+            free(msg);
+        }
+    } 
 }
